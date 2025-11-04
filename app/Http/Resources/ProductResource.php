5c18bfbb59ab3de972a564d4533ea41578fad450
Patrick Telnoni <ptelnoni89@gmail.com>
2025-11-04 12:59:33 +0700

    add foto_product to api resource

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -18,6 +18,7 @@ class ProductResource extends JsonResource
             'id'            => $this->id,
             'name'          => $this->name,
             'description'   => $this->description,
+            'foto_produk'   => $this->foto_produk
             
         ];
     }
