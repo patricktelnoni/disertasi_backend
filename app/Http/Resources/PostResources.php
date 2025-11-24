bf52916a4ecf237a348c4712f71ef6cbd59ff8a7
Patrick Telnoni <ptelnoni89@gmail.com>
2025-11-24 23:41:39 +0700

    fix product resource because of stupid ai completion

--- app/Http/Resources/PostResources.php
+++ app/Http/Resources/PostResources.php
@@ -17,7 +17,7 @@ class PostResources extends JsonResource
         return [
             'id'            => $this->id,
             'title'         => $this->title,
-            'body'          => $this->body,
+            'content'       => $this->content,
             'created_at'    => $this->created_at,
             'updated_at'    => $this->updated_at,
         ];
