2df5ae07ec1f89aaa82bf7b935ee8466a61e17bd
Patrick Telnoni <ptelnoni89@gmail.com>
2025-12-10 11:36:52 +0700

    Config sanctum guard

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Access\AuthorizationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,5 +18,20 @@ return Application::configure(basePath: dirname(__DIR__))
         App\Http\Middleware\LogRequest::class;
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // Always return JSON for unauthenticated/unauthorized API requests
+        $exceptions->render(function (AuthenticationException $e, $request) {
+            if ($request->is('api/*') || $request->expectsJson()) {
+                return response()->json([
+                    'message' => 'Unauthenticated.'
+                ], 401);
+            }
+        });
+
+        $exceptions->render(function (AuthorizationException $e, $request) {
+            if ($request->is('api/*') || $request->expectsJson()) {
+                return response()->json([
+                    'message' => 'This action is unauthorized.'
+                ], 403);
+            }
+        });
     })->create();
