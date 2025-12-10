cef1001cee1e847916e591563bca7e93860945be
Patrick Telnoni <ptelnoni89@gmail.com>
2025-12-10 12:17:33 +0700

    rollback setting

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,8 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Auth\Access\AuthorizationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,20 +16,5 @@ return Application::configure(basePath: dirname(__DIR__))
         App\Http\Middleware\LogRequest::class;
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        // Always return JSON for unauthenticated/unauthorized API requests
-        $exceptions->render(function (AuthenticationException $e, $request) {
-            if ($request->is('api/*') || $request->expectsJson()) {
-                return response()->json([
-                    'message' => 'Unauthenticated.'
-                ], 401);
-            }
-        });
-
-        $exceptions->render(function (AuthorizationException $e, $request) {
-            if ($request->is('api/*') || $request->expectsJson()) {
-                return response()->json([
-                    'message' => 'This action is unauthorized.'
-                ], 403);
-            }
-        });
+        //
     })->create();
