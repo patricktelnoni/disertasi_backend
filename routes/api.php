43735d6059f80a00bb3cceb2f34d506cafd35f96
Patrick Telnoni <ptelnoni89@gmail.com>
2025-12-30 16:34:34 +0700

    Fix curly bracket

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::apiResource('likes', 'App\Http\Controllers\Api\LikesController');
 Route::middleware(['auth:sanctum', 'check.expiration'])->group(function () {
     Route::apiResource('products', 'App\Http\Controllers\Api\ProductController');
     Route::get('/logout', 'App\Http\Controllers\Api\Auth\LoginController@_invoke');
-    Route::get('/users/{userId}/posts}', 'App\Http\Controllers\Api\PostController@getUserPosts');
+    Route::get('/users/{userId}/posts', 'App\Http\Controllers\Api\PostController@getUserPosts');
 
 });
 Route::post('/register', 'App\Http\Controllers\Api\Auth\RegisterController@_invoke');
